6f65d80a1b151339eeba33e8d7da71f9ebba2f8d
Anthony Bruno <anthonybruno@gmail.com>
2025-09-17 21:45:46 -0600

    chore: remove outdated README.md, add new project images and content, and enhance layout and styling for improved user experience

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,12 +1,23 @@
-import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
+import { defineCollection, z } from 'astro:content';
 
 const projects = defineCollection({
-  loader: glob({ pattern: '**/*.mdx', base: './src/data/projects' }),
-  schema: z.object({
-    title: z.string(),
-    slug: z.string(),
-  }),
+  loader: glob({ pattern: '**/index.mdx', base: './src/data/projects' }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      subtitle: z.string(),
+      slug: z.string(),
+      primaryImage: image().optional(),
+      links: z.array(
+        z.object({
+          url: z.string(),
+          title: z.string(),
+        }),
+      ),
+      technicalSkills: z.array(z.string()),
+      strategySkills: z.array(z.string()),
+    }),
 });
 
 export const collections = { projects };
